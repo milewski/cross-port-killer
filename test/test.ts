5e6b5cd84bf9b9f8698fe2d6930598ea7cc74f40
Rafael <rafael.milewski@gmail.com>
2017-12-30 20:39:35 +0800

    test(core): fix test

--- test/test.ts
+++ test/test.ts
@@ -4,9 +4,7 @@ import * as psList from 'ps-list'
 import { Killer } from '../source/port-killer'
 
 describe('Assassin', () => {
-
-    it('should terminate process running on a given port', () => {
-
+    it('should terminate process running on a given port', done => {
         const server = spawn('node', [ require.resolve('http-server/bin/http-server'), '-p', '7070' ], {
             stdio: 'ignore',
             detached: true
@@ -14,20 +12,20 @@ describe('Assassin', () => {
 
         server.unref()
 
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                new Killer(process.platform)
-                    .kill(7070)
-                    .then(pid => expect(server.pid.toString()).to.be(pid.pop()) && psList())
-                    .then(data => {
-                        for (let { pid } of data) expect(pid).not.to.be(server.pid)
-                    })
-                    .then(() => resolve())
-                    .catch(reject)
-            }, 1000)
-        })
+        const instance = new Killer(process.platform)
 
+        const loop = (limit = 50) => {
+            instance
+                .kill(7070)
+                .then(pid => expect(server.pid.toString()).to.be(pid.pop()) && psList())
+                .then(data => {
+                    for (let { pid } of data) expect(pid).not.to.be(server.pid)
+                })
+                .then(done)
+                .catch(() => limit > 0 ? loop(--limit) : null)
+        }
+
+        setTimeout(loop, 500)
 
     })
-
 })
